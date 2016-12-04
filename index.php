5fb3d47c29a817aa8738eddf3d0e9f193dff7b9b
haikibutsuidoushiki <idoushiki111@gmail.com>
2016-12-04 14:51:01 +0900

    php

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<?php
+
+echo "
+
+
+
+
+<html>
+<body>
+
+
+<title>idoushikis</title>
+
+
+<b>
+<a href=\"https://github.com/idoushiki/idoushikis/blob/master/php1/index.php/\">idoushikis</a>
+</b>
+
+</body>
+</html>
+
+
+
+
+
+
+";
+
+
+
+
+
+
+?>
