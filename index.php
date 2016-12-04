5271fec3c4a14f8816e6a3dd845508f6fbcd075b
haikibutsuidoushiki <idoushiki111@gmail.com>
2016-12-04 13:27:27 +0900

    index

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php
 
-$fp=fopen(https://github.com/idoushiki/idoushikis/blob/master/html1/index.html);
-var_dump(stream_get_meta_data($fp));
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<meta http-equiv="Content-Style-Type" content="text/css" />
+
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+
+<title>idoushikis</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen,tv" />
+
+</head>
+<body>
+<div id="box">
+
+<div id="header">
+
+<h1>idoushikis</h1>
+
+</div>
+
 
 
 ?>
