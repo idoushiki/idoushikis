9aae930e2c5e9f314964831701534a5947112233
haikibutsuidoushiki <idoushiki111@gmail.com>
2016-12-04 13:58:36 +0900

    indexa

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
 
-echo "<a href="https://github.com/idoushiki/idoushikis/html1/index.html">idoushikis</a>";
+echo "<a href=\"https://github.com/idoushiki/idoushikis/html1/index.html/\">idoushikis</a>";
 
 ?>
