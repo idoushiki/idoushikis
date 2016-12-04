2ba503f33ec3fa8ad3d914b845332a95d9dad432
haikibutsuidoushiki <idoushiki111@gmail.com>
2016-12-04 14:01:02 +0900

    index

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 
 <?php
 
-echo "<a href=\"https://github.com/idoushiki/idoushikis/html1/index.html/\">idoushikis</a>";
+echo "
+
+<html>
+<body>
+
+<title>idoushikis</title>
+
+
+<b>
+
+<a href=\"https://github.com/idoushiki/idoushikis/html1/index.html/\">idoushikis</a>";
+</b>
+
+
+</body>
+</html>
+
+
+
 
 ?>
