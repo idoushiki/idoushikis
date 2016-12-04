582a976d27e3362f2a1c4ce03853d23332232f54
haikibutsuidoushiki <idoushiki111@gmail.com>
2016-12-04 13:30:52 +0900

    indexa

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -11,18 +10,6 @@
 <meta http-equiv="Content-Script-Type" content="text/javascript" />
 
 <title>idoushikis</title>
-
-<link rel="stylesheet" type="text/css" href="css/style.css" media="screen,tv" />
-
-</head>
-<body>
-<div id="box">
-
-<div id="header">
-
-<h1>idoushikis</h1>
-
-</div>
 
 
 
